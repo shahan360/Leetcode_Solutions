029eabf6b6cea21735d17b7a688da19ea8edbc56
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-06-28 20:50:11 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (95.77%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -9,9 +9,11 @@ class Solution:
         if head is None:
             return None
         
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        return slow
+        slow = fast = head # humne slow aur fast pointers ko head se initialise kiya hai. (We have initialized both slow and fast pointers to the head of the linked list.)
+        # Traverse the linked list with slow moving one step and fast moving two steps
+        while fast is not None and fast.next is not None: # jab tak fast pointer None nahi hai aur fast ka next pointer bhi None nahi hai, tab tak loop chalega. (This loop will run until the fast pointer is not None and the fast's next pointer is also not None.)
+            slow = slow.next # slow pointer ko ek step aage badhaenge. (We will move the slow pointer one step forward.)
+            fast = fast.next.next # fast pointer ko do steps aage badhaenge. (We will move the fast pointer two steps forward.)
+        # When fast pointer reaches the end, slow pointer will be at the middle element
+        # jab fast pointer end tak pahunchta hai, toh slow pointer middle element par hoga. (When the fast pointer reaches the end, the slow pointer will be at the middle element
+        return slow # middle element return karega. (This will return the middle element.)
