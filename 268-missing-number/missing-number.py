b61b528a42b8df4ef7c5e13fdb4c3441474ed54b
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-07-02 23:30:14 -0500

    Time: 9 ms (28.16%) | Memory: 18.9 MB (18.23%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        total = (n * (n + 1)) // 2
-        return total - sum(nums)
+        # n = len(nums)
+        # total = (n * (n + 1)) // 2
+        # return total - sum(nums)
+
+        nums.sort()
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == mid:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left  # The missing number is at the position 'left'
         
