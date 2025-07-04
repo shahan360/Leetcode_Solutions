ade41fcec3ea91ee058492b9a79ca30ae7245fe0
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-07-04 13:04:32 -0500

    Time: 25 ms (52.75%) | Memory: 23.3 MB (79.74%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,31 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # The length of the input array
-        length = len(nums)
+        # Brute Force Approach
+        # n = len(nums) # humne nums ki length store ki hai in n (We stored the length of nums in n)
+        # result = [] # yeh result list hai jisme hum products store karenge (This is the result list where we will store the products)
+        # for i in range(n): # yeh loop i ko 0 se lekar n-1 tak iterate karega (This loop will iterate i from 0 to n-1)
+        #     product = 1 # yeh product variable hai jisme hum current index ke liye product store karenge, humne ise 1 se isliye initialize kiya hai kyunki multiplication mein 1 ka koi effect nahi hota (This is the product variable where we will store the product for the current index, initialized to 1 because multiplying by 1 does not change the product)
+        #     for j in range(n): # yeh loop j ko 0 se lekar n-1 tak iterate karega (This loop will iterate j from 0 to n-1)
+        #         if i != j: # agar i aur j alag hain toh hum product mein nums[j] ko multiply karte hain (if i and j are different, we multiply nums[j] to the product)
+        #             product *= nums[j] # humne nums[j] ko product mein multiply kiya (We multiplied nums[j] to the product)
+        #     result.append(product) # humne product ko result list mein append kiya (We appended the product to the result list)
+        # return result # agar humne result list return nahi ki toh empty list return karte hain (if we didn't return the result list, we return an empty list)
 
-        # The left and right arrays as described in the algorithm
-        L, R, answer = [0] * length, [0] * length, [0] * length
-
-        # L[i] contains the product of all the elements to the left
-        # Note: for the elemnt at index '0', there are no elements to the left,
-        # so the L[0] would be 1
-        L[0] = 1
-        for i in range(1, length):
-            # L[i-1] already contains the product of elements to the left of 'i-1'
-            # Simply multiplying it with nums[i-1] would give the product of all elements to the left of index 'i'
-            L[i] = nums[i-1]*L[i-1]
-
-        # R[i] contains the product of all the elements to the right
-        # Note: for the element at index 'length - 1', there are no elements to the right, so the R[length -1] would be 1
-        R[length - 1] = 1
-        for i in reversed(range(length - 1)):
-            # R[i+1] already contains the product of elements to the right of 'i+1'
-            # Simply multiplying it with nums[i+1] would give the product of all elements to the right of index 'i'
-            R[i] = nums[i+1] * R[i+1]
-
-        # Constructing the answer array
-        for i in range(length):
-            answer[i] = L[i]*R[i]
-
-        return answer            
+        # Optimized Approach using running product
+        # This approach uses two passes to calculate the product of all elements except the current one.
+        if len(nums) == 0 or nums is None:
+            return []
+        n = len(nums)
+        answer = [1] * n # humne answer list banayi hai jisme har element ko 1 se initialize kiya hai (We created an answer list initialized with 1 for each element)
+        running_product = 1 # yeh running_product variable hai jisme hum current product store karenge (This is the running_product variable where we will store the current product)
+        for i in range(1, n): # yeh loop i ko 1 se lekar n-1 tak iterate karega (This loop will iterate i from 1 to n-1)
+            running_product *= nums[i-1] # humne nums[i-1] ko running_product mein multiply kiya (We multiplied nums[i-1] to the running_product)
+            answer[i] = running_product # humne running_product ko answer[i] mein store kiya (We stored the running_product in answer[i])
+        running_product = 1 # ab humne running_product ko 1 se reset kiya (Now we reset the running_product to 1)
+        for i in range(n-2, -1, -1): # yeh loop i ko n-2 se lekar 0 tak iterate karega (This loop will iterate i from n-2 to 0)
+            running_product *= nums[i+1] # humne nums[i+1] ko running_product mein multiply kiya (We multiplied nums[i+1] to the running_product)
+            answer[i] *= running_product # humne answer[i] ko running_product se multiply kiya (We multiplied answer[i] with the running_product)
+        return answer # humne answer list return ki (We returned the answer list)
         
