00707afaf8e9ed2239ea8dcf9fe6cd86c6c35cad
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-08-16 15:04:07 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (49.10%) - LeetSync

--- 1090-armstrong-number/armstrong-number.py
+++ 1090-armstrong-number/armstrong-number.py
@@ -2,7 +2,7 @@ class Solution:
     def isArmstrong(self, n: int) -> bool:
         pow_a = len(str(n))
         sum_a = 0
-        original = n
+        original = n # have to store original number in a variable as once it will go inside loop it will get altered
         while n > 0:
             sum_a = sum_a + ((n%10)**pow_a)
             n = n//10
