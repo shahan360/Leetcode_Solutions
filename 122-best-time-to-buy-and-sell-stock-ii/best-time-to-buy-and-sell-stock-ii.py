c2ee414dcf6dda8b40850e177a7f443a9c6524e9
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-08-24 11:05:31 -0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (70.85%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # We will apply Greedy as it will calculate profits for only postive slopes
         buy = 0
         total = 0
-        for sell in range(1, len(prices)):
+        sell = 1
+        for sell in range(1,len(prices)):
             if prices[sell] > prices[buy]:
-                total += (prices[sell] - prices[buy])
-            buy += 1
+                profit = prices[sell] - prices[buy]
+                total += profit
+            buy +=1
         return total
-        
