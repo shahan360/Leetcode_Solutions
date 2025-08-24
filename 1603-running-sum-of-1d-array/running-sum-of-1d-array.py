0a98539a1089a6b8971f3c200d8f54517d26adad
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-08-24 17:58:25 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (70.75%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,10 +1,8 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        result = []
-        result.append(nums[0])
         for i in range(1,len(nums)):
-            result.append(result[i-1] + nums[i])
-        return result
+            nums[i] += nums[i-1]
+        return nums
 
 '''
 The error occurs because you are trying to assign a value to an index in the list result[i] that does not exist yet. You need to use append() instead of assigning by index after the first element.
