53ba13ca13353e1e13d136b07c6de3e62a01a18c
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-09-16 22:26:52 -0500

    Time: 499 ms (71.29%) | Memory: 0B (100.00%) - LeetSync

--- 1301-reformat-department-table/reformat-department-table.sql
+++ 1301-reformat-department-table/reformat-department-table.sql
@@ -1,20 +1,4 @@
 # Write your MySQL query statement below
--- select id,
-    -- SUM(CASE WHEN month = 'Jan' THEN revenue ELSE 0 END) as Jan_Revenue,
-    -- SUM(CASE WHEN month = 'Feb' THEN revenue ELSE 0 END) as Feb_Revenue,
-    -- SUM(CASE WHEN month = 'Mar' THEN revenue ELSE 0 END) as Mar_Revenue,
-    -- SUM(CASE WHEN month = 'Apr' THEN revenue ELSE 0 END) as Apr_Revenue,
-    -- SUM(CASE WHEN month = 'May' THEN revenue ELSE 0 END) as May_Revenue,
-    -- SUM(CASE WHEN month = 'Jun' THEN revenue ELSE 0 END) as Jun_Revenue,
-    -- SUM(CASE WHEN month = 'Jul' THEN revenue ELSE 0 END) as Jul_Revenue,
-    -- SUM(CASE WHEN month = 'Aug' THEN revenue ELSE 0 END) as Aug_Revenue,
-    -- SUM(CASE WHEN month = 'Sep' THEN revenue ELSE 0 END) as Sep_Revenue,
-    -- SUM(CASE WHEN month = 'Oct' THEN revenue ELSE 0 END) as Oct_Revenue,
-    -- SUM(CASE WHEN month = 'Nov' THEN revenue ELSE 0 END) as Nov_Revenue,
-    -- SUM(CASE WHEN month = 'Dec' THEN revenue ELSE 0 END) as Dec_Revenue,
--- from Department
--- group by id
-
 SELECT
   id,
     SUM(CASE WHEN month = 'Jan' THEN revenue ELSE null END) as Jan_Revenue,
