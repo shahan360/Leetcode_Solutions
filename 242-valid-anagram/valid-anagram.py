da9a2b3fd3e446010b927a7c303467a30f91b3e4
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-14 00:03:26 -0500

    Time: 11 ms (72.01%) | Memory: 18 MB (49.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,8 +3,23 @@ class Solution:
         # Brute Force Approach
         # return sorted(s) == sorted(t)
 
-        # Optimized Approach using hashmap
-        from collections import Counter
+        # Optimized Approach using Counter
+        # from collections import Counter
+        # if len(s) != len(t):
+        #     return False
+        # return Counter(s) == Counter(t)
+
+        # Optimized approach using dictionary
+
+        # Step 1: check if the length are unequal, if they are return False as unequal strings can't be anagrams
         if len(s) != len(t):
             return False
-        return Counter(s) == Counter(t)
+        
+        count = {}
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+        for ch in t:
+            if ch not in count or count[ch] == 0:
+                return False
+            count[ch] -= 1
+        return True
